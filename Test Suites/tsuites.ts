b48cb03928bf4bccec442a0f142cba9d2e6fdc41
HOAIDUC <paypal.dhtran789@gmail.com8>
2020-09-25 19:52:04 +0700

    software_testing

---
+++ Test Suites/tsuites.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>tsuites</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>214cd394-4012-4909-bad8-567f4eb09c93</testSuiteGuid>
+   <testCaseLink>
+      <guid>27988766-0d31-4b47-867a-07f0002293de</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ts5</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>86337567-b40f-4827-8295-78230ab246b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ts4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>912971aa-8ec5-4803-bfdb-bf9aa61eb3f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ts3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b5f76280-8847-44e5-9fba-a64bf7aafc49</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ts2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c053a099-6c5a-4621-bc7b-4bafb10795c6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ts1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
